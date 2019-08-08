ee259a012f922e59b3b5eb85662b2e2343929034
Roman Gruba <roman.gruba@gmail.com>
2019-08-08 16:09:49 +0300

    finished basics

--- src/index.js
+++ src/index.js
@@ -2,34 +2,44 @@ import './styles.css';
 import form from './templates/form.hbs';
 import ul from './templates/ul.hbs';
 import cards from './templates/cards.hbs';
-import fetchImg from './js/apiService';
+import imgService from './js/apiService';
+
+document.querySelector('#moreBtn').addEventListener('click', async () => {
+  await renderCards();
+  scroll();
+});
+
+document.querySelector('#queryBtn').addEventListener('click', queryUpdate);
+
+renderForm();
+renderUL();
 
 function renderForm() {
   document.querySelector('.query').insertAdjacentHTML('afterbegin', form());
 }
 
-renderForm();
-
 function renderUL() {
-  document.querySelector('body').insertAdjacentHTML('beforeend', ul());
+  document.querySelector('.query').insertAdjacentHTML('afterend', ul());
 }
 
-renderUL();
-
 async function renderCards(query) {
-  const fetchAnswer = await fetchImg(query);
+  const fetchAnswer = await imgService.fetchImg(query);
   const markup = cards(fetchAnswer);
-  document.querySelector('.gallery').insertAdjacentHTML('afterbegin', markup);
+  document.querySelector('.gallery').insertAdjacentHTML('beforeEnd', markup);
 }
 
 function queryUpdate() {
-  document.querySelector('#queryBtn').addEventListener('click', e => {
-    e.preventDefault();
-    const newQuery = document.querySelector('input[name="query"]').value;
-    renderCards(newQuery);
-  });
+  imgService.resetPage();
+  document.querySelector('.gallery').innerHTML = '';
+  const newQuery = document.querySelector('input[name="query"]').value;
+  imgService.searchQuery = newQuery;
+  renderCards(newQuery);
 }
 
-queryUpdate();
-
-// renderCards('cat');
+function scroll() {
+  let top = window.innerHeight - 50;
+  window.scrollBy({
+    top: top,
+    behavior: 'smooth',
+  });
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,10 +1,35 @@
-export default function (query) {
-  const baseUrl = 'https://pixabay.com/api/';
-  const key = '13241537-06d8871706b8937518ecf25ca';
-  let pageNumber = 1;
-  let per_page = 3;
+export default {
+  pageNumber: 1,
+  per_page: 12,
+  query: '',
+  fetchImg() {
+    const baseUrl = 'https://pixabay.com/api/';
+    const key = '13241537-06d8871706b8937518ecf25ca';
 
-  return fetch(`${baseUrl}?image_type=photo&orientation=horizontal&q=${query}&page=${pageNumber}&per_page=${per_page}&key=${key}`)
-    .then(response => response.json()).then(response => response.hits);
+    return fetch(
+      `${baseUrl}?image_type=photo&orientation=horizontal&q=${
+        this.query
+      }&page=${this.pageNumber}&per_page=${this.per_page}&key=${key}`,
+    )
+      .then(response => response.json())
+      .then(response => {
+        this.incrementPage();
+        return response.hits;
+      });
+  },
+  get searchQuery() {
+    return this.query;
+  },
 
-}
+  set searchQuery(str) {
+    this.query = str;
+  },
+
+  incrementPage() {
+    this.pageNumber += 1;
+  },
+
+  resetPage() {
+    this.pageNumber = 1;
+  }
+};
